f7ce9c644379c5d197e1ff1a2c5b2b60a6330612
Dale Severude <dseverude@msn.com>
2018-01-25 15:58:27 -0800

    Added database configuration

--- src/ComicBookGalleryModel/ComicBookGalleryModel/Context.cs
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Context.cs
@@ -10,6 +10,11 @@ namespace ComicBookGalleryModel
 {
     public class Context : DbContext
     {
+        public Context()
+        {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<Context>());
+        }
+
         public DbSet<ComicBook> ComicBooks { get; set; }
     }
 }
