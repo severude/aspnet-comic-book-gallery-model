a6d9769fe54b7623090b67314bf488844c87e21f
Dale Severude <dseverude@msn.com>
2018-01-29 11:30:25 -0800

    Added database seeding

--- src/ComicBookGalleryModel/ComicBookGalleryModel/Context.cs
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Context.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,21 @@ namespace ComicBookGalleryModel
     {
         public Context()
         {
-            Database.SetInitializer(new DropCreateDatabaseAlways<Context>());
+            Database.SetInitializer(new DatabaseInitializer());
         }
 
         public DbSet<ComicBook> ComicBooks { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            //            modelBuilder.Conventions.Remove<DecimalPropertyConvention>();
+            //            modelBuilder.Conventions.Add(new DecimalPropertyConvention(5, 2));
+
+            modelBuilder.Entity<ComicBook>()
+                .Property(cb => cb.AverageRating)
+                .HasPrecision(5, 2);
+        }
     }
 }
